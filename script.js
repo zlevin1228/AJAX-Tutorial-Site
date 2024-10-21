a589fa47aa7d3a811ff010f043df908a2182f1c1
zlevin1228 <zacharylevin1228@gmail.com>
2024-10-21 13:46:54 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var pageCounter = 1;
+var animalContainer = document.getElementById("animal-info")
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function () {
+  var ourRequest = new XMLHttpRequest();
+  ourRequest.open(
+    "GET",
+    "https://learnwebcode.github.io/json-example/animals-" + pageCounter + ".json"
+  );
+  ourRequest.onload = function () {
+    var ourData = JSON.parse(ourRequest.responseText);
+    renderHTML(ourData)
+  };
+  ourRequest.send();
+  pageCounter++;
+  if (pageCounter > 3) {
+    btn.classList.add("hide-me")
+  }
+});
+function renderHTML(data) {
+    var htmlString = ""
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+        for (ii = 0; data[i].foods.likes.length; ii++) {
+            if (ii == 0) {
+                htmlString += data[i].foods.likes[ii]
+            } else {
+                htmlString += " and " + data[i].foods.likes[ii]
+            }
+        }
+        htmlString += '.</p>'
+    }
+    animalContainer.insertAdjacentHTML('beforeend', htmlString)
+}
+
+// 35:04
